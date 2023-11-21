665cde865ebbe66923e60f9556e125e42fc27f7a
users <antenehkindu8026@gmail.com>
2023-11-21 21:19:55 +0300

    Add Google OAuth functionality

---
+++ Client/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-realstate-4abd5.firebaseapp.com",
+  projectId: "mern-realstate-4abd5",
+  storageBucket: "mern-realstate-4abd5.appspot.com",
+  messagingSenderId: "1049802887213",
+  appId: "1:1049802887213:web:3899d6053aae697912a8a4",
+  measurementId: "G-BL6D1L7P4W"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
